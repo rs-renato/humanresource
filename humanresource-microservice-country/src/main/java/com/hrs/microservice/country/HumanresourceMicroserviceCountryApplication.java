05f58d79486d579226ab0e681657e707d97b0844
Renato Rodrigues <spamcares-github@yahoo.com>
2021-10-13 22:56:18 -0300

    - humanresource-microservice-country module (initial skeleton) - pom configuration to set up parent as humanresource-parent and set up spring-boot-dependencies

---
+++ humanresource-microservice-country/src/main/java/com/hrs/microservice/country/HumanresourceMicroserviceCountryApplication.java
@@ -0,0 +1,12 @@
+package com.hrs.microservice.country;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanresourceMicroserviceCountryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanresourceMicroserviceCountryApplication.class, args);
+    }
+}
---
+++ humanresource-microservice-country/src/test/java/com/hrs/microservice/country/HumanresourceMicroserviceCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrs.microservice.country;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanresourceMicroserviceCountryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
